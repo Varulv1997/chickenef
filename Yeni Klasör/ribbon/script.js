9077592d69e6206b0304464711b1f674e209c1cb
varulv1997 <farahianas5@gmail.com>
2020-03-14 23:10:56 +0100

    First

--- Yeni Klasör/ribbon/script.js
+++
@@ -1,8 +0,0 @@
-var  body = document.getElementsByTagName('body')[0];
-var cndceShapes = document.querySelectorAll('.cndce-shape');
-
-body.onload = function(){
-	for(var i=0; i < cndceShapes.length; i++){
-		cndceShapes[i].classList.add('loaded');	
-	}
-}
